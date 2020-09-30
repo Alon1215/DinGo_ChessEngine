d78ee6820d8b58c98b57fcd2d7c1108dfa44b264
HelloWorld <michalon@post.bgu.ac.il>
2020-09-30 11:44:48 +0300

    30.9 14:46  : changed moves.go

--- moves.go
+++ moves.go
@@ -1,39 +1,91 @@
 package main
 
-//directions
+import "fmt"
+
+// directions
 const (
-	E          = 1
-	W          = -1
-	N          = 8
-	S          = -8
-	NW         = 7
-	NE         = 9
-	SW         = -NE
-	SE         = -NW
+	E  = +1
+	W  = -1
+	N  = 8
+	S  = -8
+	NW = +7
+	NE = +9
+	SW = -NE
+	SE = -NW
+
+	frMask     = 0x0000003f                 //0000 0000  0000 0000  0000 0000  0011 1111
+	toMask     = 0x00000fd0                 //0000 0000  0000 0000  0000 1111  1100 0000
+	pcMask     = 0x0000f000                 //0000 0000  0000 0000  1111 0000  0000 0000
+	cpMask     = 0x000f0000                 //0000 0000  0000 1111  0000 0000  0000 0000
+	prMask     = 0x00f00000                 //0000 0000  1111 0000  0000 0000  0000 0000
+	epMask     = 0x0f000000                 //0000 1111  0000 0000  0000 0000  0000 0000
+	castlMask  = 0xf0000000                 //1111 0000  0000 0000  0000 0000  0000 0000
+	evalMask   = uint64(0xffff000000000000) // The 16 first bits in uint64
 	toShift    = 6
-	p12Shift   = 6 + 6
-	cpShift    = 4 + 6 + 6
-	prShift    = 4 + 4 + 6 + 6
-	epShift    = 4 + 4 + 4 + 6 + 6
-	castlShift = 6 + 4 + 4 + 4 + 6 + 6
+	pcShift    = 12 //6+6
+	cpShift    = 16 //6+6+4
+	prShift    = 20 //6+6+4+4
+	epShift    = 24 //6+6+4+4+4
+	castlShift = 28 //6+6+4+4+4+4
+	evalShift  = 64 - 16
+	noMove     = move(0)
 )
 
-var pieceRules [nPt][]int //not pawns
+var pieceRules [nPt][]int // not pawns
 
 type move uint64
 
-func (m *move) packMove(fr, to, p12, cp, pr, ep int, castl castlings) {
-	// 6 bits (fr), 6 bita (to), 4 bits (p12), 4 bits (cp), 4 bits (pr), 6 bits (ep), 4 bits (castl), x bits value
-	*m = move(fr | (to << toShift) | (p12 | p12Shift) | (cp << cpShift) | (pr << prShift) | (ep << epShift) | uint(castl<<castlShift))
+func (m move) String() string {
+	s := m.StringFull()
+	s = s[1:3] + s[5:]
+	return s
+}
+func (m move) StringFull() string {
+	fr := sq2Fen[int(m.fr())]
+	to := sq2Fen[int(m.to())]
+	pc := pc2Fen(int(m.pc()))
+	cp := pc2Fen(int(m.cp())) + " "
+	pr := pc2Fen(int(m.pr()))
+	return trim(fmt.Sprintf("%v%v-%v%v%v", pc, fr, cp[:1], to, pr))
 }
 
-func init() {
-	pieceRules[Rook] = append(pieceRules[Rook], E)
-	pieceRules[Rook] = append(pieceRules[Rook], W)
-	pieceRules[Rook] = append(pieceRules[Rook], N)
-	pieceRules[Rook] = append(pieceRules[Rook], S)
+func (m *move) packMove(fr, to, pc, cp, pr, epSq int, castl castlings) {
+	// 6 bits fr, 6 bits to, 4 bits pc, 4 bits cp, 4 bits prom, 4 bits ep, 4 bits castl = 32 bits
+	if epSq == empty {
+		panic("wtf ep")
+	}
+	epFile := 0
+	if epSq != 0 {
+		epFile = epSq%8 + 1
+	}
+	*m = move(fr | (to << toShift) | (pc << pcShift) |
+		(cp << cpShift) | (pr << prShift) | (epFile << epShift) | int(castl<<castlShift))
+}
+func (m *move) packEval(score int) {
+	(*m) &= move(^evalMask) //clear eval
+	(*m) |= move(score+30000) << evalShift
 }
 
+// compare two moves - only frSq and toSq
+func (m move) cmpFrTo(m2 move) bool {
+	// return (m & move(^evalMask)) == (m2 & move(^evalMask))
+	return m.fr() == m2.fr() && m.to() == m2.to()
+}
+
+// compare two moves - only frSq, toSq and pc
+func (m move) cmpFrToP(m2 move) bool {
+	// return (m & move(^evalMask)) == (m2 & move(^evalMask))
+	return m.fr() == m2.fr() && m.to() == m2.to() && m.pc() == m2.pc()
+}
+
+// compare two moves
+func (m move) cmp(m2 move) bool {
+	return (m & move(^evalMask)) == (m2 & move(^evalMask))
+}
+
+func (m move) eval() int {
+	return int((uint(m)&uint(evalMask))>>evalShift) - 30000
+}
 func (m move) fr() int {
 	return int(m & frMask)
 }
