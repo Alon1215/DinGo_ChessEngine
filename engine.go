3b323bc7f75afcb9cdf78f6d7c2a973677ca6caf
HelloWorld <michalon@post.bgu.ac.il>
2020-10-12 16:37:44 +0300

    12.10 16:35 : create eval, update engine

--- engine.go
+++ engine.go
@@ -1,6 +1,9 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 type searchLimits struct {
 	depth    int
@@ -18,6 +21,10 @@ func (s *searchLimits) init() {
 	s.infinite = false
 }
 
+func (s *searchLimits) setStop(st bool) {
+	s.stop = st
+
+}
 func (s *searchLimits) setDepth(d int) {
 	s.depth = d
 }
@@ -26,28 +33,72 @@ func (s *searchLimits) setMoveTime(m int) {
 	s.moveTime = m
 }
 
-func engine() (frEng, toEng chan string) {
+func (s *searchLimits) setInfinite(b bool) {
+	s.infinite = b
+}
+
+func engine() (toEngine chan bool, frEngine chan string) {
 	tell("info string stopHello from engine")
-	frEng = make(chan string)
-	toEng = make(chan string)
-	go func() {
-		for cmd := range toEng {
-			switch cmd {
-			case "stop":
-			case "quit":
-			case "go":
-				tell("info string Im thinking")
-				// TODO start the thinking process in the engine from "go"
-			}
-		}
-	}()
+	frEngine = make(chan string)
+	toEngine = make(chan bool)
+	// go func() {
+	// 	for cmd := range toEng {
+	// 		switch cmd {
+	// 		case "stop":
+	// 		case "quit":
+	// 		case "go":
+	// 			tell("info string Im thinking")
+	// 			// TODO start the thinking process in the engine from "go"
+	// 		}
+	// 	}
+	// }()
+
+	go root(toEngine, frEngine)
 	return
 }
 
-func search() int {
-	return evaluate()
+func root(toEngine chan bool, frEngine chan string) {
+	b := &board
+	ml := moveList{}
+	for _ = range toEngine {
+		tell("info string engine got go! X")
+		ml = moveList{}
+		genAndSort(b, &ml)
+		for _, mv := range ml {
+			b.move(mv)
+			score := -search(b)
+			b.unmove(mv)
+
+			mv.packMove(adjEval(b, score))
+		}
+		ml.sort()
+		tell("info score cp ", fmt.Sprintf("%v", ml[0].eval()), " depth 1 pv ", ml[0].String())
+		frEngine <- fmt.Sprintf("bestmove %v%v", sq2Fen[ml[0].fr()], sq2Fen[ml[0].to()])
+	}
+
 }
 
-func evaluate() int {
-	return 0
+func search() int {
+	//return evaluate()
+	return -1
+}
+
+func genAndSort(b *boardStruct, ml *moveList) {
+	b.genAllMoves(ml)
+	//TOD: update genAllMoves()
+	for ix, mv := range *ml {
+		b.move(mv)
+		v := evaluate(b)
+		b.unmove(mv)
+		v = adjEval(b, v)
+		(*ml)[ix].packEval(v)
+	}
+	ml.sort()
+}
+
+func adjEval(b *boardStruct, ev int) int {
+	if b.stm == BLACK {
+		return -ev
+	}
+	return ev
 }
